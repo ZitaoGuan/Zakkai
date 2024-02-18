f19d0ade59cd9240dcda9f6cf00c2a8d8760dcc2
noahgiboney <noahgiboney@gmail.com>
2024-02-18 12:23:11 -0800

    feat: authenticaion for sign in and sign up. reset password sheet that sends a email to reset password

---
+++ Zakkai/Zakkai/AuthenticationManager.swift
@@ -0,0 +1,16 @@
+//
+//  Authenication.swift
+//  Zakkai
+//
+//  Created by Noah Giboney on 2/16/24.
+//
+
+import FirebaseAuth
+import Observation
+import SwiftUI
+
+
+@Observable
+class AuthenticationManager {
+    
+}
---
+++ Zakkai/Zakkai/ResetPasswordView.swift
@@ -0,0 +1,18 @@
+//
+//  ResetPasswordView.swift
+//  Zakkai
+//
+//  Created by Noah Giboney on 2/18/24.
+//
+
+import SwiftUI
+
+struct ResetPasswordView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ResetPasswordView()
+}
